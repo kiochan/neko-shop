ccc6f293977983b8e46892513b36cc30a9d930a7
kiochan <kio@kiochan.one>
2025-09-09 22:00:33 +0200

    feature: add 404 age

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Page } from "@/components/view/page";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function NotFoundPage() {
+  return (
+    <Page>
+      <Card>
+        <CardHeader>
+          <CardTitle>404</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="mb-4">
+            Sorry, the page you are looking for does not exist.
+          </p>
+          <Link href="/">
+            <Button>Go Home</Button>
+          </Link>
+        </CardContent>
+      </Card>
+    </Page>
+  );
+}
