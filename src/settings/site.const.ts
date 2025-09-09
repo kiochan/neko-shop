7136d5588ca946e8451de47ccd537fcfb88157fb
kiochan <kio@kiochan.one>
2025-09-09 22:03:34 +0200

    bugfix: use correct footer text

--- src/settings/site.const.ts
+++ src/settings/site.const.ts
@@ -2,5 +2,5 @@ export const SiteSettings = {
   name: "Neko Shop",
   title: "Neko Shop",
   description: "A e-commerce app built with Next.js",
-  footerText: "© 2025 My App",
+  footerText: "© 2025 Neko Shop. All rights reserved.",
 };
