cd17ffa14d8ade98a5ec0ff216ababba41dc0c32
Sakice <sakicezhu@gmail.com>
2025-09-18 17:13:56 +0200

    feature:import product data & build product grid mode

--- src/components/view/product-list.tsx
+++ src/components/view/product-list.tsx
@@ -1,4 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { MockProductCats } from '../mocks/product.mock.data.const';
+import ProductGridMode from './productGridMode';
 
 export default function ProductList() {
   return (
@@ -8,20 +10,7 @@ export default function ProductList() {
           <CardTitle className="text-lg font-bold text-center">Product List</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-            <div className="rounded border border-black/10 p-4 bg-black">product-1</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-2</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-3</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-4</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-5</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-6</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-7</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-8</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-9</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-10</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-11</div>
-            <div className="rounded border border-black/10 p-4 bg-black">product-12</div>
-          </div>
+          <ProductGridMode />
         </CardContent>
       </Card>
     </section>
