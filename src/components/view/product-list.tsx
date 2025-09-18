332d45e5a425112a1b174f8e31fc371789826b65
Sakice <sakicezhu@gmail.com>
2025-09-18 22:53:01 +0200

    feature: add productlistGridMode to product-list

--- src/components/view/product-list.tsx
+++ src/components/view/product-list.tsx
@@ -1,18 +1,6 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { MockProductCats } from '../mocks/product.mock.data.const';
 import ProductGridMode from './productGridMode';
 
 export default function ProductList() {
-  return (
-    <section className="mx-auto w-full max-w-4xl">
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle className="text-lg font-bold text-center">Product List</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ProductGridMode />
-        </CardContent>
-      </Card>
-    </section>
-  );
+  return <ProductGridMode />;
 }
