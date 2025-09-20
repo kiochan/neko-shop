49b39c1cdb41715230830dfb264d45166e2a560f
kiochan <kio@kiochan.one>
2025-09-21 00:42:16 +0200

    feature: impl. paginator

--- src/components/view/product-viewer-paginator.tsx
+++ src/components/view/product-viewer-paginator.tsx
@@ -1,9 +1,119 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+
 export type ProductViewerPaginatorProps = {
   offset: number;
   size: number;
   total: number;
   setPage: (page: number) => void;
 };
-export function ProductViewerPaginator({ offset, size, total }: ProductViewerPaginatorProps) {
-  return <div>TODO: Task 5: Implement your Paginator here!</div>;
+export function ProductViewerPaginator({
+  offset,
+  size,
+  total,
+  setPage,
+}: ProductViewerPaginatorProps) {
+  const currentPage = Math.floor(offset / size) + 1;
+
+  // range
+  const minPage = 1;
+  const maxPage = Math.ceil(total / size);
+
+  // how many pages should be shown before and after current page
+  const maxItemBeforeOrAfter = 2;
+
+  // pages
+  const items = new Array(maxItemBeforeOrAfter * 2 + 1)
+    .fill(0)
+    // 0 0 0 0 0
+    .map((_, index) => index)
+    // 0 1 2 3 4
+    .map((index) => index - maxItemBeforeOrAfter)
+    // -2 -1 0 1 2
+    .map((pageOffset) => pageOffset + currentPage)
+    // if current page is 5, then:
+    // 3 4 5 6 7
+    .filter((page) => page >= minPage && page <= maxPage)
+    // if only 6 pages total, then cut off
+    // 3 4 5 6
+    .map((page) => {
+      return (
+        <PaginationItem key={String(page)}>
+          <PaginationLink
+            href="#"
+            onClick={
+              page !== currentPage
+                ? () => {
+                    setPage(page);
+                  }
+                : undefined
+            }
+            isActive={page === currentPage}
+          >
+            {page}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    });
+
+  // previes/next button
+  // only display if page exists
+  const previesItem =
+    currentPage > minPage ? (
+      <PaginationItem>
+        <PaginationPrevious
+          href="#"
+          onClick={() => {
+            setPage(currentPage - 1);
+          }}
+        />
+      </PaginationItem>
+    ) : null;
+
+  const nextItem =
+    currentPage < maxPage ? (
+      <PaginationItem>
+        <PaginationNext
+          href="#"
+          onClick={() => {
+            setPage(currentPage + 1);
+          }}
+        />
+      </PaginationItem>
+    ) : null;
+
+  // ellipsis
+  // only be show if too many pages ( > maxItemBeforeOrAfter )
+  const previesEllipsis =
+    currentPage - maxItemBeforeOrAfter > minPage ? (
+      <PaginationItem>
+        <PaginationEllipsis />
+      </PaginationItem>
+    ) : null;
+
+  const nextEllipsis =
+    currentPage + maxItemBeforeOrAfter < maxPage ? (
+      <PaginationItem>
+        <PaginationEllipsis />
+      </PaginationItem>
+    ) : null;
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        {previesItem}
+        {previesEllipsis}
+        {items}
+        {nextEllipsis}
+        {nextItem}
+      </PaginationContent>
+    </Pagination>
+  );
 }
