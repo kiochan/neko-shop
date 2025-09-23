554715d5d440647f97696e4f1a862cef53a75bd9
Sakice <sakicezhu@gmail.com>
2025-09-23 18:23:42 +0200

    chore: replace magic number with two new variables

--- src/components/view/product-viewer-paginator.tsx
+++ src/components/view/product-viewer-paginator.tsx
@@ -23,6 +23,8 @@ export function ProductViewerPaginator({
 }: ProductViewerPaginatorProps) {
   const currentPage = Math.floor(offset / size) + 1;
   const totalPages = Math.ceil(total / size);
+  const homePage = 1;
+  const showPages = 4;
 
   if (totalPages < 1) {
     return null;
@@ -33,14 +35,14 @@ export function ProductViewerPaginator({
           <PaginationContent>
             <PaginationItem>
               {/** when current page is greater than 1 previous button active*/}
-              {currentPage > 1 ? (
+              {currentPage > homePage ? (
                 <PaginationPrevious onClick={() => setCurrentPage?.(currentPage - 1)} />
               ) : (
                 <PaginationPrevious />
               )}
             </PaginationItem>
             {/** when current page is greater than 1 previous page number button active */}
-            {currentPage > 1 ? (
+            {currentPage > homePage ? (
               <PaginationItem>
                 <PaginationLink onClick={() => setCurrentPage?.(currentPage - 1)}>
                   {currentPage - 1}
@@ -60,13 +62,13 @@ export function ProductViewerPaginator({
               </PaginationItem>
             ) : null}
             {/** when total pages greater than 4 show ellipsis */}
-            {totalPages > 4 ? (
+            {totalPages > showPages ? (
               <PaginationItem>
                 <PaginationEllipsis />
               </PaginationItem>
             ) : null}
             {/** when total pages greater than 4 and current page less than total pages show last page number button */}
-            {totalPages > 4 && currentPage < totalPages ? (
+            {totalPages > showPages && currentPage < totalPages ? (
               <PaginationItem>
                 <PaginationLink>{totalPages}</PaginationLink>
               </PaginationItem>
