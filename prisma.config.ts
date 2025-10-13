bc137730021ed3e5929a316b05fc3bc93c6636d8
kiochan <kio@kiochan.one>
2025-10-13 23:04:00 +0200

    chore: separate schema files

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'prisma/config'
-import 'dotenv/config'
+import dotenv from 'dotenv'
+import path from 'path'
+
+// Determine if we are in development mode
+const isDev = process.env.NODE_ENV !== 'production'
+
+// Load .env or .env.production dynamically
+const envFile = isDev ? path.resolve('.env') : path.resolve('.env.production')
+
+dotenv.config({ path: envFile })
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'prisma/schema',
   migrations: {
     seed: 'tsx prisma/seed.ts',
   },
