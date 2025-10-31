74df5663aa3c5ec891b1eebed182dfc5f59c507f
kiochan <kio@kiochan.one>
2025-10-31 03:39:25 +0100

    fix: params should be awaited before using

--- src/app/product/[page]/page.tsx
+++ src/app/product/[page]/page.tsx
@@ -1,10 +1,12 @@
 import { ProductViewer } from '@/features/product-viewer'
 import { PageContainer } from '@/shared/layout/page-container'
 
-export default async function ProductPage({ params }: { params: { page: string } }) {
+export default async function ProductPage({ params }: { params: Promise<{ page: string }> }) {
+  const { page } = await params
+
   return (
     <PageContainer>
-      <ProductViewer page={Number(params.page)} path="/product" />
+      <ProductViewer page={Number(page)} path="/product" />
     </PageContainer>
   )
 }
