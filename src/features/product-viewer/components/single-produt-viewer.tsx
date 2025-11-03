94c0b289889418a6266afdf642cdb538e0f3ab5f
Sakice <sakicezhu@gmail.com>
2025-11-03 21:26:13 +0100

    fix: adjust layout and styling for product detail page

--- src/features/product-viewer/components/single-produt-viewer.tsx
+++ src/features/product-viewer/components/single-produt-viewer.tsx
@@ -28,14 +28,13 @@ export async function ProductDetailPage({ id }: ProductId) {
       <div className="flex flex-col mb-4 md:flex-row gap-6 ">
         <div className="grid product-single w-full">
           <div className="grid_item md:col-span-1 flex flex-col">
-            <div className="w-full border rounded-lg items-center justify-center">
+            <div className="w-full rounded-lg flex items-center justify-center">
               <img
                 src={product.imageUrl || 'image not found'}
                 alt={product.name}
                 width={800}
                 height={800}
-                sizes="(min-width: 1280px) 560px, (min-width: 1024px) 420px, (min-width: 768px) 360px, 240px"
-                className="w-full h-full object-contain"
+                className="w-164 h-164 aspect-square object-cover"
               />
             </div>
           </div>
@@ -43,8 +42,6 @@ export async function ProductDetailPage({ id }: ProductId) {
         <div className="grid product-single">
           <div className="grid_item md:col-span-1 flex flex-col">
             <h1 className="text-xl md:text-2xl font-bold text-left mb-6">{product.name}</h1>
-            <h2 className="text-lg font-semibold mb-2">Description</h2>
-            <p className="text-gray-600 whitespace-pre-line mb-4">{product.description}</p>
             <p className="text-lg text-gray-700 mb-4">
               {Intl.NumberFormat(undefined, { style: 'currency', currency: 'USD' }).format(
                 product.price
@@ -56,6 +53,8 @@ export async function ProductDetailPage({ id }: ProductId) {
               Add to Cart
             </Button>
             <Button className="flex w-full mb-4 sm:w-auto max-w-full">Buy Now</Button>
+            <h2 className="text-lg font-semibold mb-2">Description</h2>
+            <p className="text-gray-600 whitespace-pre-line mb-4">{product.description}</p>
           </div>
         </div>
       </div>
